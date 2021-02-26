4d91af96d7256b9d00d9ad401b9a02bacfc8ff75
jdcdp <47483528+jdcdp@users.noreply.github.com>
2021-02-26 20:16:44 +0100

    Update mission_setup.sh

--- mission_setup.sh
+++ mission_setup.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
 sudo apt install ros-melodic-desktop-full
+sudo apt install python3-argcomplete
+echo 'alias ros_src="source /opt/ros/melodic/setup.bash"' >> ~/.bashrc
+source .bashrc
+ros_src
