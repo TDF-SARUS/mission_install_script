127d74e25e32c647d1f83684de58ec6950e3f318
jdcdp <47483528+jdcdp@users.noreply.github.com>
2021-02-27 11:40:46 +0100

    Update mission_setup.sh

--- mission_setup.sh
+++ mission_setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-apt-key adv --fetch-keys 'https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc'
+sudo apt-key adv --fetch-keys 'https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc'
+wget -O - https://drive.upm.es/index.php/s/XLOZT2jqh77rvf7/download?path=public | sudo apt-key add -
 apt-add-repository 'http://packages.ros.org/ros/ubuntu'
 sudo sh -c 'echo "deb https://drive.upm.es/index.php/s/XLOZT2jqh77rvf7/download?path=/dists/bionic ./" > /etc/apt/sources.list.d/aerostack-latest.list'
-wget -O - https://drive.upm.es/index.php/s/XLOZT2jqh77rvf7/download?path=public | sudo apt-key add -
 sudo apt update
 sudo apt install ros-melodic-desktop-full -y
 sudo apt install git -y
