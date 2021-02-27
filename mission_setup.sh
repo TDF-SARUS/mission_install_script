3b135d1b27e1413d283885755916947f7155fe86
jdcdp <47483528+jdcdp@users.noreply.github.com>
2021-02-27 11:25:55 +0100

    Update mission_setup.sh

--- mission_setup.sh
+++ mission_setup.sh
@@ -4,9 +4,9 @@ apt-add-repository 'http://packages.ros.org/ros/ubuntu'
 sudo sh -c 'echo "deb https://drive.upm.es/index.php/s/XLOZT2jqh77rvf7/download?path=/dists/bionic ./" > /etc/apt/sources.list.d/aerostack-latest.list'
 wget -O - https://drive.upm.es/index.php/s/XLOZT2jqh77rvf7/download?path=public | sudo apt-key add -
 sudo apt update
-sudo apt install ros-melodic-desktop-full
-sudo apt install git
-sudo apt install python3-argcomplete
-sudo apt install aerostack-melodic
+sudo apt install ros-melodic-desktop-full -y
+sudo apt install git -y
+sudo apt install python3-argcomplete -y
+sudo apt install aerostack-melodic -y
 
 
